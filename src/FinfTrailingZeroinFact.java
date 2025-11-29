43e1456d29e19ba860d51f63e205a2959da5ce7d
shoaib <shasanex@gmail.com>
2025-11-29 10:49:47 +0530

    Finf trailing zero in factorial number.

---
+++ src/FinfTrailingZeroinFact.java
@@ -0,0 +1,10 @@
+public class FinfTrailingZeroinFact {
+    static void main() {
+        int n = 30;  //30 fact
+        int rs = 0;
+        for (int i = 5; i<=n; i = i*5) {
+            rs = rs + n/i;
+        }
+        System.out.println("Trailing Zero in Fact : "+rs);
+    }
+}
