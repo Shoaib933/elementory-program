3a056432dceb10ed936123361f0cd2c582225e76
shoaib <shasanex@gmail.com>
2025-11-30 12:44:15 +0530

    Prime Number.

---
+++ src/PrimeNumber.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class PrimeNumber {
+    static boolean[] sieveOfEratosthenes(int n) {
+        boolean[] isPrime = new boolean[n+1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for(int i=2; i*i <=n; i++) {
+            for (int j=2*i; j<=n; j += i) {
+                isPrime[j] = false;
+            }
+        } return isPrime;
+    }
+}
+
+void main() {
+    boolean[] isPrime = PrimeNumber.sieveOfEratosthenes(20);
+    for (int i=0; i<=20; i++){
+        System.out.println(i+" : " +isPrime[i]);
+    }
+
+}
