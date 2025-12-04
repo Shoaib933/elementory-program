5f1f5d52b64bf44fdb53406da3d4ca2718daa500
shoaib <shasanex@gmail.com>
2025-12-04 12:37:51 +0530

    Commit :9 - Another approach to sort zero one array.

--- src/lovebabbar/arrays/SortZeoresAndOnes.java
+++ src/lovebabbar/arrays/SortZeoresAndOnes.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class SortZeoresAndOnes {
     static void main() {
         int[] arr = {0,1,0,1,1,0,1,0,1,1};
+        /*
         int s = 0;
         int e = arr.length-1;
         int i = 0;
@@ -24,5 +25,26 @@ public class SortZeoresAndOnes {
            }
        }
         Arrays.stream(arr).forEach(a -> System.out.print(a+","));
+*/
+        leftShift(arr);
+    }
+
+    static void leftShift(int[] arr) {
+        int l=0; int h = arr.length-1;
+        while(l<h) {
+            if (arr[l]==0) {
+                l++;
+            } else if (arr[h] == 1) {
+                h--;
+            } else {
+                int temp = arr[l];
+                arr[l] = arr[h] ;
+                arr[h] = temp;
+            }
+        }
+        System.out.println("Another approach");
+        Arrays.stream(arr).forEach((a)-> System.out.print(" "+a));
+
+
     }
 }
